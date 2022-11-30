8eb8f5aaa44f367fa78744c2efe46040f4ed60a4
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2022-11-30 00:39:01 +0000

    initial_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import SocialMedia from "./components/SocialMedia";
+import Timeline from "./components/Timeline";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="welcom_page">
+        <div className="pict_component">
+          <div className="principal_pict"></div>
+          <div className="socialmedia_component">
+            <SocialMedia/>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="education_page">
+        <Timeline/>
+        
+      </div>
+      <div className="projects_page">
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SocialMedia = () => {
+
+    return (
+        <div className="social-network">
+            <a className="social-network-logo" id="social-network-facebook" href="https://duckduckgo.com"> </a>
+            <a className="social-network-logo" id="social-network-linkedin" href="https://duckduckgo.com"> </a>
+            <a className="social-network-logo" id="social-network-phone" href="https://duckduckgo.com"> </a>
+            <a className="social-network-logo" id="social-network-mail" href="https://duckduckgo.com"> </a>
+        </div>
+    );
+} 
+
+export default SocialMedia;
