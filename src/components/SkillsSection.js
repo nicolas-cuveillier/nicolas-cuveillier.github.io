6b386262f20d40feca19b34a6893d64126445d6f
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2022-12-11 18:58:05 +0000

    skills selectors

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -14,12 +14,25 @@ function SkillsSection() {
         }        
     },[playOnce]);
 
+    function handleSelection(id) {
+        const elems = document.getElementsByClassName("skills-selectioner")
+        for (var i = 0; i<3; i++) {
+            elems[i].style.backgroundColor = ''
+        }
+        document.getElementById(id).style.backgroundColor = 'rgba(128, 128, 128, 1)'
+        //setdata(data.filter(e => ))
+    }
+
     return(
         <section name="section3" className="skills-page">
             <div className="skills-intro-container">
                 <div className="skills-title-selection-container">
                     <div className="skills-title">Skills</div>
-                    <div className="skills-selection-container"></div>
+                    <div className="skills-selection-container">
+                        <div className="skills-selectioner" id="select1" onClick={() => handleSelection("select1")}>tecnical</div>
+                        <div className="skills-selectioner" id="select2" onClick={() => handleSelection("select2")}>soft</div>
+                        <div className="skills-selectioner" id="select3" onClick={() => handleSelection("select3")}>test</div>
+                    </div>
                 </div>
             </div>
             <div className="skills-items-container">
