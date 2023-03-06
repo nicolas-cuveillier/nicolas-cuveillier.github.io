c9733141483547fa88113ac7f71a2684cbc2f930
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2023-03-06 19:25:49 +0000

    refactoring text

--- src/components/WelcomSection.js
+++ src/components/WelcomSection.js
@@ -10,8 +10,12 @@ function WelcomPage(){
             <div className="pict_component">
                 <div className="presentation-container">
                     <h2>Hello !</h2>
-                    <p>I'm Nicolas, a computer science student and here is a bit of my life.</p>
-                    <p>I'm Nicolas, a computer science student and here is a bit of my life.</p>
+                    <p id="p1">
+                        I'm Nicolas, a computer science student and here is a bit of my life.</p>
+                    <p id="p2">
+                        I'm currently studying abroad in Portugal for the last year of my bachelor and I am looking for 
+                        an internship either in Cybersecurity or Data Science.
+                    </p>
                 </div>
                 <div className="socialmedia_component">
                     <SocialMedia/>
