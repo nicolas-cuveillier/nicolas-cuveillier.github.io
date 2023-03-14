003527090017a21d6855883bccfc8083e64d30e2
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2023-03-14 22:40:10 +0000

    positioning elems in project section

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -3,7 +3,11 @@ import React from "react";
 function ProjectSection() {
     return(
         <section name="section4" className="projects_page">
-        
+            <h1>Project's title</h1>
+            <div className="project-content-container">
+                <div className="project-content-video-container"></div>
+                <div className="project-content-text-container"></div>
+            </div>
         </section>
     )
 }
