3f631d168d316a1c241ea49106fa865bce25a847
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2023-05-06 16:40:05 +0100

    setup deployement

---
+++ build/precache-manifest.40468c33252e2490a1d67b6f21a9bfa3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9c2ed0b865b9dd29370d019b1ad51a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5457f41c2a68ee549acc",
+    "url": "/static/css/main.4598f74a.chunk.css"
+  },
+  {
+    "revision": "0708c9b3f240905d1b63",
+    "url": "/static/js/2.1d24e7e4.chunk.js"
+  },
+  {
+    "revision": "d42d166beebf0036812df944f314de23",
+    "url": "/static/js/2.1d24e7e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8347363b02bfb1cd33",
+    "url": "/static/js/3.494ff62d.chunk.js"
+  },
+  {
+    "revision": "5457f41c2a68ee549acc",
+    "url": "/static/js/main.0a213e62.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0a213e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729e05a97ab4327bce9d",
+    "url": "/static/js/runtime-main.c84a1f06.js"
+  },
+  {
+    "revision": "be1fe3134d5bff7fb99d621201de9aed",
+    "url": "/static/media/HelveticaNowDisplay-Bold.be1fe313.ttf"
+  },
+  {
+    "revision": "3f5d59dd15ec5db603a5779417fc49f6",
+    "url": "/static/media/Logo.3f5d59dd.png"
+  },
+  {
+    "revision": "178416d493eb1077163d4dc8a7b58fee",
+    "url": "/static/media/NCCV.178416d4.pdf"
+  },
+  {
+    "revision": "b7150cb8f5886006045be99c3083a5a1",
+    "url": "/static/media/profilepict.b7150cb8.png"
+  },
+  {
+    "revision": "ced26cf7c7eac98435342195c9cc5027",
+    "url": "/static/media/project-picture1.ced26cf7.png"
+  },
+  {
+    "revision": "8506171e35f1bb71996e82173c7ba807",
+    "url": "/static/media/project-picture2.8506171e.png"
+  }
+]);
